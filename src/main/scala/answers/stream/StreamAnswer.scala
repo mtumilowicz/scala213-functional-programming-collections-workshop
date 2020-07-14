2437fdd99c95690e67c2011be85b00b360cddab9
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-07-14 10:49:06 +0200

    StreamFp -> StreamAnswer

---
+++ src/main/scala/answers/stream/StreamAnswer.scala
@@ -0,0 +1,201 @@
+package answers.stream
+
+sealed trait StreamAnswer[+A] {
+
+  def toList: List[A] = {
+    @scala.annotation.tailrec
+    def loop(stream: StreamAnswer[A], list: List[A] = List()): List[A] = {
+      stream match {
+        case EmptyStreamAnswer => list.reverse
+        case ConsStreamAnswer(h, t) => loop(t(), h() :: list)
+      }
+    }
+
+    loop(this)
+  }
+
+  def drop(n: Int): StreamAnswer[A] = {
+    @scala.annotation.tailrec
+    def loop(stream: StreamAnswer[A], n: Int): StreamAnswer[A] = {
+      if (n == 0) return stream
+      stream match {
+        case EmptyStreamAnswer => stream
+        case ConsStreamAnswer(_, t) => loop(t(), n - 1)
+      }
+    }
+
+    loop(this, n)
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case ConsStreamAnswer(h, t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
+
+  def take(n: Int): StreamAnswer[A] = {
+    def listToStream: List[A] => StreamAnswer[A] =
+      _.foldLeft(StreamAnswer.empty[A])((stream, elem) => StreamAnswer.cons(elem, stream))
+
+    @scala.annotation.tailrec
+    def loop(stream: StreamAnswer[A], n: Int, list: List[A] = List()): List[A] = {
+      if (n == 0) return list
+      stream match {
+        case EmptyStreamAnswer => list
+        case ConsStreamAnswer(h, t) => loop(t(), n - 1, h() :: list)
+      }
+    }
+
+    listToStream(loop(this, n))
+  }
+
+  def takeWhile(p: A => Boolean): StreamAnswer[A] = {
+    def listToStream: List[A] => StreamAnswer[A] =
+      _.foldLeft(StreamAnswer.empty[A])((stream, elem) => StreamAnswer.cons(elem, stream))
+
+    @scala.annotation.tailrec
+    def loop(stream: StreamAnswer[A], taken: List[A] = List()): StreamAnswer[A] = {
+      stream match {
+        case EmptyStreamAnswer => listToStream(taken)
+        case ConsStreamAnswer(h, t) if p(h()) => loop(t(), h() :: taken)
+        case ConsStreamAnswer(h, t) if !p(h()) => loop(t(), taken)
+      }
+    }
+
+    loop(this)
+  }
+
+  def find(p: A => Boolean): Option[A] =
+    filter(p).headOption
+
+  def forAll(p: A => Boolean): Boolean = {
+    foldRight(true)((newElem, forAll) => p(newElem) && forAll)
+  }
+
+  def takeWhileFold(p: A => Boolean): StreamAnswer[A] = {
+    foldRight(StreamAnswer.empty[A])((newElem, taken) => if (p(newElem))
+      StreamAnswer.cons(newElem, taken)
+    else StreamAnswer.empty)
+  }
+
+  def headOption: Option[A] = {
+    foldRight(Option.empty[A])((newElem, _) => Some(newElem))
+  }
+
+  def map[B](f: A => B): StreamAnswer[B] = {
+    /*
+    this match {
+      case EmptyStreamFp => EmptyStreamFp
+      case ConsStreamFp(h, t) => StreamFp.cons(f(h()), t().map(f))
+    }
+     */
+
+    /*
+
+    Cons(1, Cons(2, Cons(3, Empty))).map(_ * 2)
+    Cons(1, Cons(2, Cons(3, Empty))).foldRight(Empty)((next, mapped) => StreamFp.cons(next * 2, mapped))
+    StreamFp.cons(2, Cons(2, Cons(3, Empty)).foldRight(Empty)(...))
+
+     */
+    foldRight(StreamAnswer.empty[B])((next, mapped) => StreamAnswer.cons(f(next), mapped))
+  }
+
+  def filter(p: A => Boolean): StreamAnswer[A] = {
+    foldRight(StreamAnswer.empty[A])((h, t) =>
+      if (p(h)) StreamAnswer.cons(h, t)
+      else t)
+  }
+
+  def append[B >: A](s: => StreamAnswer[B]): StreamAnswer[B] = {
+    foldRight(s)((newElem, appended) => StreamAnswer.cons(newElem, appended))
+  }
+
+  def flatMap[B](f: A => StreamAnswer[B]): StreamAnswer[B] = {
+    foldRight(StreamAnswer.empty[B])((newElem, flat) => f(newElem) append flat)
+  }
+
+  def mapUnfold[B](f: A => B): StreamAnswer[B] = {
+    StreamAnswer.unfold(this) {
+      case ConsStreamAnswer(h, t) => Some((f(h()), t()))
+      case _ => None
+    }
+  }
+
+  def startsWith[B >: A](s: StreamAnswer[B]): Boolean = {
+    @scala.annotation.tailrec
+    def loop(list: StreamAnswer[A], sequence: StreamAnswer[B] = s): Boolean = {
+      sequence match {
+        case EmptyStreamAnswer => true
+        case ConsStreamAnswer(headS, tailS) => list match {
+          case EmptyStreamAnswer => false
+          case ConsStreamAnswer(head, tail) => if (head() == headS()) loop(tail(), tailS()) else false
+        }
+      }
+    }
+
+    loop(this)
+  }
+
+  def tails: StreamAnswer[StreamAnswer[A]] = {
+    StreamAnswer.unfold(this) {
+      case EmptyStreamAnswer => Option.empty
+      case ConsStreamAnswer(h, t) => Option.apply((ConsStreamAnswer(h, t), t()))
+    } append StreamAnswer.cons(StreamAnswer.empty, StreamAnswer.empty)
+  }
+
+  def scanRight[B](z: B)(f: (A, => B) => B): StreamAnswer[B] = {
+    foldRight((z, StreamAnswer.cons(z, StreamAnswer.empty)))((a, p0) => {
+      val b2 = f(a, p0._1)
+      (b2, StreamAnswer.cons(b2, p0._2))
+    })._2
+  }
+}
+
+case object EmptyStreamAnswer extends StreamAnswer[Nothing]
+
+case class ConsStreamAnswer[+A](h: () => A, t: () => StreamAnswer[A]) extends StreamAnswer[A]
+
+object StreamAnswer {
+  def cons[A](hd: => A, tl: => StreamAnswer[A]): StreamAnswer[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    ConsStreamAnswer(() => head, () => tail)
+  }
+
+  def empty[A]: StreamAnswer[A] = EmptyStreamAnswer
+
+  def apply[A](as: A*): StreamAnswer[A] =
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+  def constant[A](a: A): StreamAnswer[A] = {
+    StreamAnswer.cons(a, constant(a))
+  }
+
+  def from(n: Int): StreamAnswer[Int] = {
+    StreamAnswer.cons(n, from(n + 1))
+  }
+
+  def fibs(): StreamAnswer[Int] = {
+    def fib(prev: Int, next: Int, stream: => StreamAnswer[Int] = StreamAnswer.empty): StreamAnswer[Int] = {
+      StreamAnswer.cons(prev, fib(next, prev + next, stream))
+    }
+
+    fib(0, 1)
+  }
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): StreamAnswer[A] = {
+    f(z) match {
+      case Some((a, s)) => StreamAnswer.cons(a, unfold(s)(f))
+      case None => EmptyStreamAnswer
+    }
+
+  }
+
+  def fibUnfold(): StreamAnswer[Int] = {
+    unfold((0, 1)) { case (prev, next) => Some(prev, (next, prev + next)) }
+  }
+
+}
---
+++ src/test/scala/answers/stream/StreamAnswerTest.scala
@@ -0,0 +1,166 @@
+package answers.stream
+
+class StreamAnswerTest extends org.scalatest.FunSuite with org.scalatest.matchers.should.Matchers {
+
+  test("toList") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.toList == List(1, 2, 3)
+    StreamAnswer.empty.toList shouldBe List()
+  }
+
+  test("take") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.take(0).toList shouldBe List()
+    stream.take(1).toList shouldBe List(1)
+    stream.take(3).toList shouldBe List(1, 2, 3)
+    stream.take(4).toList shouldBe List(1, 2, 3)
+    StreamAnswer.empty.take(1).toList shouldBe List()
+  }
+
+  test("drop") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.drop(0).toList shouldBe List(1, 2, 3)
+    stream.drop(1).toList shouldBe List(2, 3)
+    stream.drop(3).toList shouldBe List()
+    stream.drop(4).toList shouldBe List()
+    StreamAnswer.empty.drop(1).toList shouldBe List()
+  }
+
+  test("takeWhile") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.takeWhile(_ < -1).toList shouldBe List()
+    stream.takeWhile(_ <= 1).toList shouldBe List(1)
+    stream.takeWhile(_ <= 3).toList shouldBe List(1, 2, 3)
+    stream.takeWhile(_ <= 4).toList shouldBe List(1, 2, 3)
+    StreamAnswer.empty[Int].takeWhile(_ < 1).toList shouldBe List()
+  }
+
+  test("forAll") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.forAll(_ > 0) shouldBe true
+    stream.forAll(_ > 3) shouldBe false
+    StreamAnswer.empty[Int].forAll(_ > 3) shouldBe true
+  }
+
+  test("takeWhileFold") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.takeWhileFold(_ < -1).toList shouldBe List()
+    stream.takeWhileFold(_ <= 1).toList shouldBe List(1)
+    stream.takeWhileFold(_ <= 3).toList shouldBe List(1, 2, 3)
+    stream.takeWhileFold(_ <= 4).toList shouldBe List(1, 2, 3)
+    StreamAnswer.empty[Int].takeWhile(_ < 1).toList shouldBe List()
+  }
+
+  test("headOption") {
+    val stream = StreamAnswer(1, 2, 3)
+    val infinite = StreamAnswer.constant(1)
+
+    stream.headOption shouldBe Some(1)
+    StreamAnswer.empty[Int].headOption shouldBe None
+    infinite.headOption shouldBe Some(1)
+  }
+
+  test("map") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.map(_ + 1).toList shouldBe List(2, 3, 4)
+    StreamAnswer.empty[Int].map(_ + 1).toList shouldBe List()
+  }
+
+  test("map is lazy") {
+    val infinite = StreamAnswer.constant(1)
+
+    infinite.map(_ * 2)
+  }
+
+  test("filter") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.filter(_ >= 2).toList shouldBe List(2, 3)
+    StreamAnswer.empty[Int].filter(_ > 1).toList shouldBe List()
+  }
+
+  test("append") {
+    val stream = StreamAnswer(1, 2, 3)
+    val stream2 = StreamAnswer(4, 5)
+
+    stream.append(stream2).toList == List(1, 2, 3, 4, 5)
+    stream2.append(stream).toList == List(4, 5, 1, 2, 3)
+    StreamAnswer.empty.append(stream).toList == List(1, 2, 3)
+    stream.append(StreamAnswer.empty).toList == List(1, 2, 3)
+  }
+
+  test("flatMap") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.flatMap(i => StreamAnswer.cons(i, StreamAnswer.cons(i, StreamAnswer.empty))).toList shouldBe List(1, 1, 2, 2, 3, 3)
+    stream.flatMap(_ => StreamAnswer.empty).toList shouldBe List()
+    StreamAnswer.empty[Int].flatMap(_ => StreamAnswer.cons(1, StreamAnswer.empty)).toList == List()
+  }
+
+  test("mapUnfold") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.mapUnfold(_ + 1).toList shouldBe List(2, 3, 4)
+    StreamAnswer.empty[Int].mapUnfold(_ + 1).toList shouldBe List()
+  }
+
+  test("constant") {
+    val stream = StreamAnswer.constant(1)
+
+    stream.take(3).toList shouldBe List(1, 1, 1)
+  }
+
+  test("from") {
+    val stream = StreamAnswer.from(1)
+
+    stream.take(0).toList shouldBe List()
+    stream.take(3).toList shouldBe List(1, 2, 3)
+  }
+
+  test("fibs") {
+    val stream = StreamAnswer.fibs()
+
+    stream.take(0).toList shouldBe List()
+    stream.take(5).toList shouldBe List(0, 1, 1, 2, 3)
+  }
+
+  test("fibsUnfold") {
+    val stream = StreamAnswer.fibUnfold()
+
+    stream.take(0).toList shouldBe List()
+    stream.take(5).toList shouldBe List(0, 1, 1, 2, 3)
+  }
+
+  test("startsWith") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.startsWith(stream) shouldBe true
+    stream.startsWith(StreamAnswer(1, 2)) shouldBe true
+    stream.startsWith(StreamAnswer(1)) shouldBe true
+    stream.startsWith(StreamAnswer.empty) shouldBe true
+    stream.startsWith(StreamAnswer(1, 2, 4)) shouldBe false
+    stream.startsWith(StreamAnswer(2)) shouldBe false
+    stream.startsWith(StreamAnswer(1, 2, 3, 4)) shouldBe false
+  }
+
+  test("tails") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.tails.toList.map(_.toList) shouldBe List(List(1, 2, 3), List(2, 3), List(3), List())
+    StreamAnswer.empty[Int].tails.toList.map(_.toList) shouldBe List(List())
+  }
+
+  test("scanRight") {
+    val stream = StreamAnswer(1, 2, 3)
+
+    stream.scanRight(0)(_ + _).toList shouldBe List(6, 5, 3, 0)
+    StreamAnswer.empty[Int].scanRight(0)(_ + _).toList shouldBe List(0)
+  }
+}
